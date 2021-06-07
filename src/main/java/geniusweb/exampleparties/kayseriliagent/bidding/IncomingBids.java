be5754fba2eb7ce72e0549cbee327fff28cc5595
mustafabaris <mustafa.camli@ozu.edu.tr>
2021-06-07 18:27:31 +0300

    opponent modelling added

---
+++ src/main/java/geniusweb/exampleparties/kayseriliagent/bidding/IncomingBids.java
@@ -0,0 +1,56 @@
+package geniusweb.exampleparties.kayseriliagent;
+
+
+import geniusweb.issuevalue.Value;
+
+import java.util.*;
+
+public class IncomingBids {
+    private Collection<Value> issueValueList;
+    private Double totalOccurrence;
+    private ArrayList<Collection<Value>> issuesList = new ArrayList<Collection<Value>>();
+    private HashMap<String, HashMap<String, Double>> bidsHistory;
+
+
+    public IncomingBids(Collection<Value> issueValueList, Double occurrence, HashMap<String, HashMap<String, Double>> bidsHistory) {
+        this.issueValueList = issueValueList;
+        this.totalOccurrence = occurrence;
+        this.issuesList.add(issueValueList);
+        this.bidsHistory = bidsHistory;
+
+    }
+    public void printBids(){
+        for (Collection<Value> issue : issuesList){
+
+            System.out.println(issue + " : " + this.totalOccurrence);
+        }
+        calculateFrequency();
+    }
+     public void calculateFrequency(){
+        HashMap<String,Double> values = new HashMap<>();
+        ArrayList<HashMap<String,Double>> valuesList = new ArrayList<HashMap<String,Double>>();
+         double sum = 0.0;
+         int size = 0;
+
+             for (String k : bidsHistory.keySet()) {
+                 if (k != null)
+                      values = bidsHistory.get(k);
+                 System.out.println("Values: " + values);
+                 valuesList.add(values);
+             }
+             for (Collection<Value> issue: issuesList){
+                 size += issue.size();
+                 for (int i = 0; i<issue.size(); i++){
+                    String issueVal = issue.toArray()[i].toString();
+                 for (HashMap<String,Double> tmpValues: valuesList){
+                        if (tmpValues.containsKey(issueVal)){
+                            System.out.println(issueVal + " Double Value: " + tmpValues.get(issueVal));
+                            sum += tmpValues.get(issueVal);
+                        }
+                    }
+                 }
+             }
+             System.out.println("Total Sum: " + sum / size / this.totalOccurrence+"\n-----------------");
+         }
+
+}
