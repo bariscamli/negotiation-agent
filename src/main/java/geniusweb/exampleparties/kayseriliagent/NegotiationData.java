2dda211ea7695ff1853f25ff59fb0f91b2010627
Baris Camli <mustafa.camli@ozu.edu.tr>
2021-06-08 17:26:09 +0300

    Update NegotiationData.java

--- src/main/java/geniusweb/exampleparties/kayseriliagent/NegotiationData.java
+++ src/main/java/geniusweb/exampleparties/kayseriliagent/NegotiationData.java
@@ -3,6 +3,9 @@ package geniusweb.exampleparties.kayseriliagent; // TODO: change name
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * The class hold the negotiation data that is obtain during a negotiation
  * session. It will be saved to disk after the negotiation has finished. During
@@ -13,34 +16,32 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 @JsonAutoDetect(fieldVisibility = Visibility.ANY)
 public class NegotiationData {
 
-    private Double maxReceivedUtil = 0.0;
-    private Double agreementUtil = 0.0;
+    private ArrayList<ArrayList<Double>> bidsHistory = new ArrayList<ArrayList<Double>>();
+    private int totalNegotation = 0;
+
     private String opponentName;
 
-    public void addAgreementUtil(Double agreementUtil) {
-        this.agreementUtil = agreementUtil;
-        if (agreementUtil > maxReceivedUtil)
-            this.maxReceivedUtil = agreementUtil;
-    }
-
-    public void addBidUtil(Double bidUtil) {
-        if (bidUtil > maxReceivedUtil)
-            this.maxReceivedUtil = bidUtil;
+    public void addOpponentBidUtil(Double bidUtil) {
+        this.bidsHistory.get(this.totalNegotation).add(bidUtil);
     }
 
     public void setOpponentName(String opponentName) {
         this.opponentName = opponentName;
     }
 
+    public void settotalNegotation(int num) {
+        this.totalNegotation += num;
+    }
+
     public String getOpponentName() {
         return this.opponentName;
     }
 
-    public Double getMaxReceivedUtil() {
-        return this.maxReceivedUtil;
+    public int gettotalNegotation() {
+        return this.totalNegotation;
     }
 
-    public Double getAgreementUtil() {
-        return this.agreementUtil;
+    public ArrayList<ArrayList<Double>> getbidsHistory() {
+        return this.bidsHistory;
     }
 }
