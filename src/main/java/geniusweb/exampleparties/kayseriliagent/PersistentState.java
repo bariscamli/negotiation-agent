dc6df5aaa3cf61b4f7b385d834b4fcf4592ac3c3
Baris Camli <mustafa.camli@ozu.edu.tr>
2021-06-08 17:22:30 +0300

    Update PersistentState.java

--- src/main/java/geniusweb/exampleparties/kayseriliagent/PersistentState.java
+++ src/main/java/geniusweb/exampleparties/kayseriliagent/PersistentState.java
@@ -1,5 +1,6 @@
 package geniusweb.exampleparties.kayseriliagent; // TODO: change name
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +22,7 @@ public class PersistentState {
     private Map<String, Double> avgMaxUtilityOpponent = new HashMap<String, Double>();
     private Map<String, Integer> opponentEncounters = new HashMap<String, Integer>();
 
+
     /**
      * Update the persistent state with a negotiation data of a previous negotiation
      * session
@@ -30,25 +32,80 @@ public class PersistentState {
      */
     public void update(NegotiationData negotiationData) {
         // Keep track of the average utility that we obtained
-        this.averageUtility = (this.averageUtility * negotiations + negotiationData.getAgreementUtil())
-                / (negotiations + 1);
+
+        double mean_five = 0.0;
+        double mean_seven = 0.0;
+        double mean_eight = 0.0;
+        double mean_else = 0.0;
+
+        double std_five = 0.0;
+        double std_seven = 0.0;
+        double std_eight = 0.0;
+        double std_else = 0.0;
+
+        ArrayList<Double> array = new ArrayList<Double>();
+        array.add(0.5);
+        array.add(0.7);
+        array.add(0.8);
+        array.add(1.0);
+
+        int size_five = 0;
+        int size_seven = 0;
+        int size_eight = 0;
+        int size_else = 0;
+
+        for (int i = 0; i < negotiationData.gettotalNegotation() ; i++) {
+            for(int j = 0; j < negotiationData.getbidsHistory().get(i).size(); j++) {
+                if (j <= negotiationData.getbidsHistory().get(i).size() * 0.8)
+                    mean_five += negotiationData.getbidsHistory().get(i).get(j);
+                    size_eight +=1;
+                if (j <= negotiationData.getbidsHistory().get(i).size() * 0.7)
+                    mean_seven += negotiationData.getbidsHistory().get(i).get(j);
+                    size_seven +=1;
+                if (j <= negotiationData.getbidsHistory().get(i).size() * 0.5)
+                    mean_eight += negotiationData.getbidsHistory().get(i).get(j);
+                    size_five +=1;
+                else
+                    mean_else += negotiationData.getbidsHistory().get(i).get(j);
+                    size_else +=1;
+            }
+        }
+
+        mean_five /=  size_five;
+        mean_seven /=  size_seven;
+        mean_eight /=  size_eight;
+        mean_else /=  size_else;
+
+        for (int i = 0; i < negotiationData.gettotalNegotation() ; i++) {
+            for(int j = 0; j < negotiationData.getbidsHistory().get(i).size(); j++) {
+                if (j <= negotiationData.getbidsHistory().get(i).size() * 0.8)
+                    std_eight += Math.abs(negotiationData.getbidsHistory().get(i).get(j) - mean_eight);
+                if (j <= negotiationData.getbidsHistory().get(i).size() * 0.7)
+                    std_seven += Math.abs(negotiationData.getbidsHistory().get(i).get(j) - mean_seven);
+                if (j <= negotiationData.getbidsHistory().get(i).size() * 0.5)
+                    std_five += Math.abs(negotiationData.getbidsHistory().get(i).get(j) - mean_five);
+                else
+                    std_else += Math.abs(negotiationData.getbidsHistory().get(i).get(j) - mean_else);
+            }
+        }
+
+        std_five = Math.sqrt(std_five / size_five);
+        std_seven = Math.sqrt(std_five / size_seven);
+        std_eight = Math.sqrt(std_five / size_eight);
+        std_else = Math.sqrt(std_five / size_else);
+
+
 
         // Keep track of the number of negotiations that we performed
-        negotiations++;
+        negotiationData.gettotalNegotation()
 
         // Get the name of the opponent that we negotiated against
         String opponent = negotiationData.getOpponentName();
 
         // Check for safety
         if (opponent != null) {
-            // Update the number of encounters with an opponent
-            Integer encounters = opponentEncounters.containsKey(opponent) ? opponentEncounters.get(opponent) : 0;
-            opponentEncounters.put(opponent, encounters + 1);
-            // Track the average value of the maximum that an opponent has offered us across
-            // multiple negotiation sessions
-            Double avgUtil = avgMaxUtilityOpponent.containsKey(opponent) ? avgMaxUtilityOpponent.get(opponent) : 0.0;
-            avgMaxUtilityOpponent.put(opponent,
-                    (avgUtil * encounters + negotiationData.getMaxReceivedUtil()) / (encounters + 1));
+		// std dusuyorsa -> competitive davraniyor 
+		// std yükseliyorsa -> social davraniyor
         }
     }
 
@@ -66,14 +123,7 @@ public class PersistentState {
         return null;
     }
 
-    public boolean knownOpponent(String opponent) {
+    public Boolean knownOpponent(String opponent) {
         return opponentEncounters.containsKey(opponent);
-    }
-
-    public boolean isCompetitive() {
-        return true;
-    }
-    public boolean isSocial(){
-        return false;
     }
 }
